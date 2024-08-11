c96da96c14dbcc5e60371e2b210f58ef6aba1c95
Jeremias Naves de Freitas <jnfbrofficial@gmail.com>
2024-08-11 17:14:30 -0300

    Início do desenvolvimento da Home Page

---
+++ lacia-web/src/components/HomePage.vue
@@ -0,0 +1,26 @@
+---------------------------------------TEMPLATE---------------------------------------
+<template>
+        <header id="topBar" class="topBarClass"> </header> 
+        <h1>Bem-vindo ao Meu Site</h1>
+        <p>Esta é a página inicial do meu site criado com Vue.js.</p>
+
+
+        
+</template>
+
+---------------------------------------SCRIPTS----------------------------------------
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+
+----------------------------------------STYLES----------------------------------------
+<style>
+.topBarClass{
+  justify-content: flex-start;
+  height: 100px;
+  background-color: #000912;
+}
+</style>
